25c879054cbae4b5de4e0fabeee7c099f9546e52
CraciDenisStefan <crdenis13@gmail.com>
2023-08-29 15:47:29 +0200

    Recupero dati con axios ok + creato loader

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,28 +1,47 @@
 <script>
 import axios from 'axios';
-
+import AppLoader from './AppLoader.vue';
 export default {
     name: 'AppMain',
+    components:{
+        AppLoader
+    },
     
     data(){
         return{
-
+            baseUrl: 'http://127.0.0.1:8000',
+            projects: [],
+            loading: true,
         }
     },
     created(){
-
+        this.getProjects();
     },
     methods: {
-        
+        getProjects(){
+            this.loading = true;
+
+            axios.get(`${this.baseUrl}/api/projects`).then((response)=>{
+
+                if(response.data.status){
+                    this.projects = response.data.results;
+                    this.loading =false;
+                }
+                else{
+
+                }
+            })
+        }
     },
     
 }
 </script>
 <template lang="">
-    <div>
-        prova
+    <div class="container">
+        provadsafa
+        <AppLoader />
     </div>
 </template>
-<style lang="">
+<style lang="scss">
     
 </style>
