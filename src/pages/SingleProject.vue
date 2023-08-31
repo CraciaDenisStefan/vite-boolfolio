d54f765b2147273ae38a82536cb3a9d075cad6a7
CraciDenisStefan <crdenis13@gmail.com>
2023-08-31 14:51:20 +0200

    Creato pagina del progetto singolo

---
+++ src/pages/SingleProject.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from 'axios';
+import { store } from '../store.js'
+
+export default {
+    name:'SingleProject',
+    data(){
+        return{
+            store,
+            project: null,
+        }
+    },
+    created(){
+
+    },
+    methods:{
+
+    }
+}
+</script>
+<template >
+    <div>
+        Projetto singolo
+    </div>
+</template>
+<style lang="scss">
+    
+</style>
