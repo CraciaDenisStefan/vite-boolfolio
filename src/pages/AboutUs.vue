4b0c0df8d9a4c75bfca76cb82a66ce269e22848e
CraciDenisStefan <crdenis13@gmail.com>
2023-09-01 15:08:14 +0200

    creato pagina about us

---
+++ src/pages/AboutUs.vue
@@ -0,0 +1,22 @@
+<script>
+import AskForm from '../components/AskForm.vue';
+
+export default {
+    components: {
+        AskForm,
+    }
+}
+</script>
+<template >
+    <div class="container">
+        <div class="row">
+            <div class="col-12 ">
+                <h1 class="text-center">Chi siamo ?</h1>
+            </div>
+        </div>
+        <AskForm/>
+    </div>
+</template>
+<style lang="scss">
+
+</style>
